628e8ca86c62dd5cd791c86eb1be6691ddb395b4
Raily <59217215+Raily-j3@users.noreply.github.com>
2021-01-30 14:23:20 +0800

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 import os
+import sys
 import base64
 import requests
 import re
 import json
 
+
+CONFIG_FILE_PATH = '/usr/local/etc/v2ray/config.json'
 URL = 'https://dingyue.space/link/aaITN8EcHAh4CVmr?sub=3&extend=1'
 
 
@@ -22,35 +25,59 @@ def getVmess():
     return vmess_list_decode
 
 
-def modifyConfig(address, port, id):
-    config_file_path = '/etc/v2ray/config.json'
-    # config_file_path = 'config.json'
-    with open(config_file_path, 'r+') as config_file:
+def modifyConfig(address, port, aid, id):
+    with open(CONFIG_FILE_PATH, 'r+') as config_file:
         config_json = json.load(config_file)
         config_json['outbounds'][0]['settings']['vnext'][0]['address'] = address
         config_json['outbounds'][0]['settings']['vnext'][0]['port'] = port
-        config_json['outbounds'][0]['settings']['vnext'][0]['users'][0][id] = id
+        config_json['outbounds'][0]['settings']['vnext'][0]['users'][0]['id'] = id
+        config_json['outbounds'][0]['settings']['vnext'][0]['users'][0]['alterId'] = aid
 
         config_file.truncate(0)
         config_file.seek(0)
         json.dump(config_json, config_file)
 
-        
 
-if __name__ == '__main__':
-    vmess_list = getVmess()
-
+def interface(vmess_list):
+    # list all selections
     for i in range(len(vmess_list)):
         print("{}-{}".format(i, vmess_list[i]['ps']))
 
-    choice_num = int(input("选择一个服务器\n"))
+    while True:
+        print('=' * 40)
+        choice_num = input("选择一个服务器 (quit input 'q')\n")
 
-    while choice_num >= len(vmess_list) or choice_num < 0:
-        print('输入有误，请输入正确数字。')
-        choice_num = int(input("选择一个服务器\n"))
+        if choice_num.isdigit():
+            choice_num = int(choice_num)
+            if choice_num >= len(vmess_list) or choice_num < 0:
+                print('输入有误，请输入正确数字。')
+            else:
+                choice = vmess_list[choice_num]
+                modifyConfig(choice['add'], choice['port'], choice['aid'], choice['id'])
+                os.system('systemctl restart v2ray.service')
+
+                return 0
+        else:
+            if choice_num == "q":
+                sys.exit()
+            else:
+                print("Please select a right number!")
+
+    # if not choice_num.isdigit():
+    #     if choice_num == "q":
+    #         sys.exit()
+    #         print()
+    # else:
+    #     while choice_num >= len(vmess_list) or choice_num < 0:
+    #         print('输入有误，请输入正确数字。')
+    #         choice_num = int(input("选择一个服务器\n"))
+
+
+def main():
+    vmess_list = getVmess()
+    interface(vmess_list)
+
+
+if __name__ == '__main__':
+    main()
     
-    choice = vmess_list[choice_num]
-
-    modifyConfig(choice['add'], choice['port'], choice['id'])
-
-    os.system('systemctl restart v2ray.service')
